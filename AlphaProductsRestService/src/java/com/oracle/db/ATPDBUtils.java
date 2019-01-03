f931993026c1402c827907eb3369b412d01e1575
Sasanka Abeysinghe <sasanka.abeysinghe@ttu.edu>
2019-01-03 15:46:16 -0600

    Get param updates

--- AlphaProductsRestService/src/java/com/oracle/db/ATPDBUtils.java
+++ AlphaProductsRestService/src/java/com/oracle/db/ATPDBUtils.java
@@ -96,6 +96,77 @@ public class ATPDBUtils {
         }
         return products;
     }
+    public static JSONArray getProducts(int prodID) {
+    readProperties();
+    JSONArray products = new JSONArray();
+        try {
+            OracleDataSource ODS = new OracleDataSource();
+
+            ODS.setURL("jdbc:oracle:thin:@" + INSTANCE + "?TNS_ADMIN=" + CREDENTIALS);
+            ODS.setUser(USER);
+            ODS.setPassword(PASSWORD);
+            //ODS.getConnection();
+            Connection conn = ODS.getConnection();
+            System.out.println("Connection test Succeeded. You are connected to ATP as Admin!");
+            Statement stmt = conn.createStatement();
+
+            // Select the ENAME column from the EMP table
+            ResultSet rset = stmt.executeQuery("SELECT * FROM PRODUCTS WHERE PRODUCT_ID="+prodID);
+            JSONObject obj;// = new JSONObject();
+            
+            //products.add("PRODUCT_ID "+":"+"1039");
+            //products.add("PARENT_CATEGORY_ID"+":"+"1039");
+            //products.add("CATEGORY_ID"+":"+"1039");
+            //obj.put("Products", products);
+
+            // Iterate through the result and print the employee names
+            while (rset.next()) {
+                obj = new JSONObject();
+                obj.put("PRODUCT_ID", rset.getInt("PRODUCT_ID"));
+
+                obj.put("PARENT_CATEGORY_ID", rset.getInt("PARENT_CATEGORY_ID"));
+                obj.put("CATEGORY_ID", rset.getInt("CATEGORY_ID"));
+                obj.put("PRODUCT_NAME", rset.getString("PRODUCT_NAME"));
+                obj.put("PRODUCT_STATUS", rset.getString("PRODUCT_STATUS"));
+                obj.put("COST_PRICE", rset.getInt("COST_PRICE"));
+                obj.put("LIST_PRICE", rset.getInt("LIST_PRICE"));
+                obj.put("MIN_PRICE", rset.getInt("MIN_PRICE"));
+                obj.put("WARRANTY_PERIOD_MONTHS", rset.getInt("WARRANTY_PERIOD_MONTHS"));
+                obj.put("EXTERNAL_URL", rset.getString("EXTERNAL_URL"));
+                obj.put("ATTRIBUTE_CATEGORY", rset.getString("ATTRIBUTE_CATEGORY"));
+                obj.put("ATTRIBUTE1", rset.getString("ATTRIBUTE1"));
+                obj.put("ATTRIBUTE2", rset.getString("ATTRIBUTE2"));
+                obj.put("ATTRIBUTE3", rset.getString("ATTRIBUTE3"));
+                obj.put("ATTRIBUTE4", rset.getString("ATTRIBUTE4"));
+                obj.put("ATTRIBUTE5", rset.getString("ATTRIBUTE5"));
+                obj.put("CREATED_BY", rset.getString("CREATED_BY"));
+                obj.put("CREATION_DATE", rset.getString("CREATION_DATE"));
+                obj.put("LAST_UPDATED_BY", rset.getString("LAST_UPDATED_BY"));
+                obj.put("LAST_UPDATE_DATE", rset.getString("LAST_UPDATE_DATE"));
+                obj.put("OBJECT_VERSION_ID", rset.getInt("OBJECT_VERSION_ID"));
+
+                products.add(obj);
+
+                System.out.println("Product ID is " + rset.getString(1) + " " + rset.getString(2) + " " + rset.getString(3) + " " + " " + rset.getString(4));
+
+            }
+            // Close the RseultSet
+            rset.close();
+            rset = null;
+
+            // Close the Statement
+            stmt.close();
+            stmt = null;
+            
+            conn.close();
+            return products;
+
+        } catch (SQLException e) {
+            System.out.println("Connection Unsuccessful with errror " + e.getMessage());
+            e.printStackTrace();
+        }
+        return products;
+    }
     private static void readProperties() {
         Properties prop = new Properties();
         InputStream input = null;
--- AlphaProductsRestService/src/java/com/oracle/db/RestCallResource.java
+++ AlphaProductsRestService/src/java/com/oracle/db/RestCallResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import com.oracle.db.ATPDBUtils;
+import javax.ws.rs.PathParam;
 
 /**
  * REST Web Service
@@ -42,6 +43,13 @@ public class RestCallResource {
         //TODO return proper representation object
         return ATPDBUtils.getProducts().toJSONString();
     }
+    @GET
+    @Path("/{PRODUCT_ID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson(@PathParam("PRODUCT_ID") int id){
+        return ATPDBUtils.getProducts(id).toJSONString();
+    }
+    
 
     /**
      * PUT method for updating or creating an instance of RestCallResource
